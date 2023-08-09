dee5b24cc5b275cc49c3fbbf6b27de354bc0a84b
robotAstray <robotastray@cantab.net>
2023-08-09 19:29:05 +0100

    updating python versions in setup.py
    Update to the classifiers section to include "Programming Language :: Python :: 3.11" to match the requested support versions. In addition "Programming Language :: Python :: 3.7" was removed

--- setup.py
+++ setup.py
@@ -39,10 +39,10 @@ setuptools.setup(
         "Operating System :: MacOS",
         "Operating System :: POSIX :: Linux",
         "Operating System :: Microsoft :: Windows",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Scientific/Engineering :: Physics",
     ],
 )
